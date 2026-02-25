0b3dba1c4186597a7d84e734870611804d55a789
mutantfrogs <themutantfrogs@gmail.com>
2026-02-24 19:08:38 -0500

    feat: fresh start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,35 @@
-import { HashRouter as Router, Routes, Route} from "react-router-dom";
-import React, { useState } from 'react';
-import AboutPage from './pages/AboutPage';
-import GalleryPage from './pages/GalleryPage';
-import BlogPage from './pages/BlogPage';
-import Header from './components/Header'
-import './styles/styles.css'
-import "xp.css/dist/XP.css";
-import ViewNotesPage from "./pages/ViewNotesPage";
-import CreateNotesPage from "./pages/CreateNotesPage";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-    return(
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<AboutPage title="about - mutantfrogs"/>}/>
-                <Route path="/explore" element={<GalleryPage title="explore - mutantfrogs"/>}/>
-                <Route path="/blog" element={<BlogPage title="blog - mutantfrogs"/>}/>
-                <Route path="/notes/view" element={<ViewNotesPage title="notes - mutantfrogs"/>}/>
-                <Route path="/notes/create" element={<CreateNotesPage title="notes - mutantfrogs"/>}/>
-            </Routes>
-        </Router>
-    )
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- server/models/notes.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const NotesSchema = new mongoose.Schema({
-    img: String,
-    comment: String
-})
-
-const NotesModel = mongoose.model("notes", NotesSchema);
-module.exports = NotesModel;
