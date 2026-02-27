9eff34ab68bd1af9f65799984feaaa8a0cb845e4
mutantfrogs <themutantfrogs@gmail.com>
2026-02-26 23:01:14 -0500

    feat: add specific splastext date support

--- client/src/components/Splashtext.jsx
+++ client/src/components/Splashtext.jsx
@@ -7,8 +7,21 @@ export default function Splashtext() {
   const [fontsize, setFontsize] = useState(null);
 
   useEffect(() => {
-    let index = Math.floor(Math.random() * splashlist.length)
-    let text = splashlist[index].value
+    let now = new Date(Date.now())
+    let month = now.getMonth() + 1
+    let day = now.getDate()
+
+    let filteredSplashlist
+    filteredSplashlist = splashlist.filter(s => s.month == month && s.day == day)
+    if(filteredSplashlist.length == 0){
+      filteredSplashlist = splashlist.filter(s => s.month == month && s.day == undefined)
+      if(filteredSplashlist.length == 0){
+        filteredSplashlist = splashlist.filter(s => s.month == undefined && s.day == undefined)
+      }
+    }
+    
+    let index = Math.floor(Math.random() * filteredSplashlist.length)
+    let text = filteredSplashlist[index].value
     let size = Math.max(16,(40 - text.length * 0.6))
     setSplashtext(text)
     setFontsize(size)
