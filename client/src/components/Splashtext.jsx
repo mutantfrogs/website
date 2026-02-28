3a3654683969b796a3d33f870b98ac56dfd7f4e4
mutantfrogs <themutantfrogs@gmail.com>
2026-02-27 20:12:25 -0500

    feat: add clickable splashes and refactor filter logic

--- client/src/components/Splashtext.jsx
+++ client/src/components/Splashtext.jsx
@@ -1,35 +1,46 @@
 import {useState, useEffect} from 'react';
-import splashlist from '../assets/splashtext.json'
+import splashes from '../assets/splashtext.json'
 
 export default function Splashtext() {
+  const [currentSplash, setCurrentSplash] = useState(null);
 
-  const [splashtext, setSplashtext] = useState(null);
-  const [fontsize, setFontsize] = useState(null);
-
-  useEffect(() => {
+  //returns list of splashes available for the current date
+  const filterSplashes = () => {
     let now = new Date(Date.now())
     let month = now.getMonth() + 1
     let day = now.getDate()
 
-    let filteredSplashlist
-    filteredSplashlist = splashlist.filter(s => s.month == month && s.day == day)
-    if(filteredSplashlist.length == 0){
-      filteredSplashlist = splashlist.filter(s => s.month == month && s.day == undefined)
-      if(filteredSplashlist.length == 0){
-        filteredSplashlist = splashlist.filter(s => s.month == undefined && s.day == undefined)
-      }
+    //decrease granularity of filters
+    const filters = [
+      s => s.month === month && s.day === day,
+      s => s.month === month && s.day == null,
+      s => s.month == null && s.day == null
+    ];
+
+    //keep applying filters until one returns a value
+    for(const filter of filters){
+      const result = splashes.filter(filter);
+      if (result.length) return result;
     }
-    
-    let index = Math.floor(Math.random() * filteredSplashlist.length)
-    let text = filteredSplashlist[index].value
-    let size = Math.max(16,(40 - text.length * 0.6))
-    setSplashtext(text)
-    setFontsize(size)
+
+    //default value (should never happen)
+    return [{value:"Something went horribly wrong..."}]
+  }
+
+  //pick value from filtered list
+  const loadSplash = () => {
+    const filteredSplashes = filterSplashes()
+    let index = Math.floor(Math.random() * filteredSplashes.length)
+    setCurrentSplash(filteredSplashes[index].value)
+  }
+
+  useEffect(() => {
+    loadSplash()
   }, [])
 
   return (
-    <h4 className='splashtext' style={{fontSize: fontsize}}>
-        {splashtext}
+    <h4 className='splashtext' onClick={loadSplash}>
+        {currentSplash}
     </h4>
   )
 }
